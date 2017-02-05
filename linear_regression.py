87c21fab7277489e5036f844cb73cc2adfb48547
Jalen Green <jalen.g@gmail.com>
2017-02-04 22:22:48 -0800

    Submission generation and linear regression
    The linear regression submission got 0.28363 score
    on the public leaderboards.
    I rounded the predictions to 1 or 2, though the model
    predictions without rounding seemed to always be
    very large (x10^8) positive or negative.
    
    Did not expect performance to be great, but not this bad.

--- linear_regression.py
+++ linear_regression.py
@@ -3,20 +3,65 @@ import numpy as np
 from pkl_help import get_pkl
 from pkl_help import read_make_pkl
 from sklearn.linear_model import LinearRegression
+import sys
+import getopt
+from submission import make_submission_2008
+from submission import make_submission_2012
+
+
 # Get training data
 X_train_2008 = get_pkl("saved_objs/X_train_2008.pkl")
-Y_train_2008 = get_pkl("saved_objs/X_train_2008.pkl")
+Y_train_2008 = get_pkl("saved_objs/Y_train_2008.pkl")
+X_test_2008 = get_pkl("saved_objs/X_test_2008.pkl")
+X_test_2012 = get_pkl("saved_objs/X_test_2012.pkl")
+
 
 # Write function to generate model
 def gen_lin_reg_model():
     model = LinearRegression()
     model.fit(X_train_2008, Y_train_2008)
+    # DEBUG
+    print("d0: " + str(Y_train_2008.shape))
+    print("d1: " + str(model.predict(X_test_2008).shape))
+    print("d2: " + str(model.coef_.shape))
+    #
     return model
+
+def round_to_1_or_2(a):
+    if (a[0] > 1.5):
+        return 2
+    else:
+        return 1
+
+def lin_reg_modified_predict(model, Y):
+    preds = model.predict(Y).reshape(-1, 1)
+    mpreds = np.apply_along_axis(round_to_1_or_2, 1, preds)
+    # Debug
+    print("mpreds.shape: " + str(mpreds.shape))
+    return mpreds
 
 # Save model
 lin_reg_model = read_make_pkl("saved_objs/lin_reg.pkl", gen_lin_reg_model)
 
-# Write function to output predictions on 2008 test data
-
-# Write function to output predictions on 2012 test data
-
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"ho:",["output="])
+    except getopt.GetoptError:
+        print 'linear_regression.py [-o [2008] [2012]]'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'linear_regression.py [-o [2008] [2012]]'
+            sys.exit()
+        elif opt in ("-o", "--output"):
+            if (arg == "2008"):
+                # DEBUG
+                print("lin_reg_model.predict(X_test_2008).shape" +
+                      str(lin_reg_model.predict(X_test_2008).shape))
+                make_submission_2008("submissions/linear_regression_2008.csv",
+                                     lin_reg_modified_predict(lin_reg_model, X_test_2008))
+            elif (arg == "2012"):
+                make_submission_2012("submissions/linear_regression_2012.csv",
+                                     lin_reg_modified_predict(lin_reg_model, X_test_2012))
+if __name__ == "__main__":
+    main(sys.argv[1:])
